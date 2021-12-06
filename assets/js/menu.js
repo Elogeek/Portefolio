91612445598384a80129c7889578ea53a626c6bc
elogeek <elodiechristin@gmail.com>
2021-12-06 17:52:31 +0100

    start portfolio

---
+++ assets/js/menu.js
@@ -0,0 +1,65 @@
+const Menu = function(liens) {
+    this.parent = document.querySelector('nav');
+    this.liens = liens;
+
+    this.build = function() {
+        this.ul = document.createElement('ul');
+        this.ul.className = "menu-list"
+        for(let lien of this.liens) {
+            const li = document.createElement('li');
+            li.appendChild(lien.getHtmlLinkElement())
+            this.ul.appendChild(li);
+        }
+        this.parent.appendChild(this.ul);
+    }
+
+    this.attachClickEvent = function(){
+        for(let a of this.liens) {
+            a.getHtmlLinkElement().addEventListener('click', function(event) {
+                event.preventDefault();
+                console.log('lien ' + a.titre + ' a été cliqué !');
+            });
+        }
+    }
+
+}
+
+/**
+ * A single link HtmlElement.
+ * @param lien
+ * @param titre
+ * @constructor
+ */
+const Link = function(lien, titre) {
+    this.lien = lien;
+    this.titre = titre;
+    this.aHtmlElement = null;
+
+    this.getHtmlLinkElement = function() {
+        if(this.aHtmlElement === null) {
+            this.aHtmlElement = document.createElement('a');
+            this.aHtmlElement.innerHTML = this.titre;
+            this.aHtmlElement.title = this.titre;
+            this.aHtmlElement.href = this.lien;
+        }
+        return this.aHtmlElement;
+    }
+}
+
+let iconMenu = document.querySelector("#openMenu");
+iconMenu.addEventListener("click", ev => {
+    iconMenu.style.display ="none";
+    // Menu parent element.
+    const myMenu = document.querySelector('nav');
+    const linksMenu = [
+        new Link("#", "Home"),
+        new Link("#", "Works"),
+        new Link("#", "About me"),
+        new Link("#", "Contact")
+    ];
+
+    const a = new Menu(myMenu,linksMenu);
+    a.build();
+    a.attachClickEvent();
+
+})
